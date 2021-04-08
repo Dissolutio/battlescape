4945fde60ac0f044009b3702202177ab819255ab
John Moen <entity.john@gmail.com>
2021-04-08 17:22:30 -0500

    refactor

--- src/game/armyGen.ts
+++ src/game/armyGen.ts
@@ -1,34 +1,30 @@
 import { hexedMeadowCards } from "./cards"
 import { ArmyCard, GameArmyCard, GameUnits, GType_Armies } from "./types"
 
-// Start with standard army cards
 const hexedMeadowCardsArr: ArmyCard[] = Object.values(hexedMeadowCards)
-
-// Transform standard cards into game-cards
-const armyCards: GameArmyCard[] = hexedMeadowCardsArr.map(fillGameCardInfo)
-
-// Transform game-cards into game-units
-// todo this could use some params, so some units can be pre-dead
+const armyCards: GameArmyCard[] = hexedMeadowCardsArr.map(
+  (card: GameArmyCard) => {
+    const isCardABee = card.race === "bee"
+    const isCardAButterfly = card.race === "butterfly"
+    const playerID = isCardABee ? "0" : isCardAButterfly ? "1" : ""
+    // id factory
+    function makeGameCardID() {
+      return `p${playerID}_${card.armyCardID}`
+    }
+    const gameCard: GameArmyCard = {
+      ...card,
+      playerID,
+      cardQuantity: 1,
+      gameCardID: makeGameCardID(),
+    }
+    return gameCard
+  }
+)
 const gameUnits = armyCardsToGameUnits(armyCards)
 
 export const initialArmies: GType_Armies = {
   armyCards,
   gameUnits,
-}
-function fillGameCardInfo(card: GameArmyCard): GameArmyCard {
-  const isCardABee = card.race === "bee"
-  const isCardAButterfly = card.race === "butterfly"
-  const playerID = isCardABee ? "0" : isCardAButterfly ? "1" : ""
-  // id factory ...
-  function makeGameCardID() {
-    return `p${playerID}_${card.armyCardID}`
-  }
-  return {
-    ...card,
-    playerID,
-    cardQuantity: 1,
-    gameCardID: makeGameCardID(),
-  }
 }
 export function armyCardsToGameUnits(cards: GameArmyCard[]): GameUnits {
   // id factory
