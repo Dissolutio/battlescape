654d2d1f25afa945b9c854bd561eb8b5f257b35d
John Moen <entity.john@gmail.com>
2021-04-11 11:34:18 -0500

    tidy refactor of theme player colors

--- src/theme.ts
+++ src/theme.ts
@@ -3,38 +3,27 @@ import { makeHexagonsHeroPatternDataUrl } from "assets/hexagonsHeroPatternDataUr
 
 export const colors = {
   gray: "#5d576b",
-  grayUrlEncoded: "%235d576b",
   // player colors
   beeYellow: "#E0BB00",
-  beeYellowUrlEncoded: "%23E0BB00",
   butterflyPurple: "#fc65b8",
-  butterflyPurpleUrlEncoded: "%23fc65b8",
   waspRed: "#E4572E",
-  waspRedUrlEncoded: "%23E4572E",
   beetleBlue: "#058ed9",
-  beetleBlueUrlEncoded: "%23058ed9",
   hummingbirdGreen: "#75DBCD",
-  hummingbirdGreenUrlEncoded: "%2375DBCD",
-  humansGoldenrod: "#E8AA14",
-  humansGoldenrodUrlEncoded: "%23E8AA14",
-}
-const playerColorsUrlEncoded = {
-  "0": colors.beeYellowUrlEncoded,
-  "1": colors.butterflyPurpleUrlEncoded,
-  "2": colors.waspRedUrlEncoded,
-  "3": colors.beetleBlueUrlEncoded,
-  "4": colors.hummingbirdGreen,
-  "5": colors.humansGoldenrodUrlEncoded,
+  humansPurple: "#8900f2",
 }
 
 const bgContourLines = (playerID: string) =>
-  contourLinesBG(playerColorsUrlEncoded[playerID], "0.1")
+  contourLinesBG(encodeURIComponent(playerColors[playerID]), "0.1")
 const hexSvgBgUrl = (playerID: string) =>
   makeHexagonsHeroPatternDataUrl(playerColors[playerID], 0.2)
 
 const playerColors = {
   "0": colors.beeYellow,
   "1": colors.butterflyPurple,
+  "2": colors.waspRed,
+  "3": colors.beetleBlue,
+  "4": colors.hummingbirdGreen,
+  "5": colors.humansPurple,
 }
 
 export const theme = (playerID: string) => {
@@ -42,7 +31,7 @@ export const theme = (playerID: string) => {
     colors,
     playerColors,
     playerColor: playerColors[playerID],
-    playerColorUrlEncoded: encodeURIComponent(playerColorsUrlEncoded[playerID]),
+    playerColorUrlEncoded: encodeURIComponent(playerColors[playerID]),
     bgContourLinesUrl: bgContourLines(playerID),
     hexSvgBgUrl: hexSvgBgUrl(playerID),
   }
